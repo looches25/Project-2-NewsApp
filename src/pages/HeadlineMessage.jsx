935ab54b4780b7f0a469c068a8eca3b7fe6422ff
liyic25 <liyic25@gmail.com>
2022-06-06 11:20:03 +0800

    updated Date presentation

--- src/pages/HeadlineMessage.jsx
+++ src/pages/HeadlineMessage.jsx
@@ -1,12 +1,18 @@
-
 function HeadlineMesssage () {
+    
+    let  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const current = new Date();
-    const date =`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
+    const day = current.getDate()
+    const month = months [current.getMonth()]
+    const year = current.getFullYear()
+
+    const date =`${current.getDate()} ${month} ${current.getFullYear()}`
     const time =`${current.getHours()}:${current.getMinutes()+1}:${current.getSeconds()}`
     return (
         <div className="headline-message">
         <h1> Latest Headlines </h1>
-        <p> Last Updated:{date} {time} </p>
+        <p> Last Updated: {date} at {time} </p>
+
         </div>
     )
 }
